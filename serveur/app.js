bcb6005856ab12c5faae5af143dda6bd9e8c2c08
Jean-Robert CRENN <jean-robert.crenn@epitech.eu>
2020-01-15 15:23:51 +0100

    new server

--- serveur/app.js
+++ serveur/app.js
@@ -1,40 +1,46 @@
+var createError = require('http-errors');
 var express = require('express');
-var mongoose = require('mongoose')
-var session = require('express-session')
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
-var passport = require('passport')
-var cors = require('cors')
-var bodyParser = require('body-parser')
 
 var port = process.env.PORT || 8080;
 
-var configDB = require('./config/database');
-mongoose.connect(configDB.url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-require('./config/passport')(passport)
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.use(cors())
-
-app.use('/', require('./routes/routes'));
-app.use(express.static('public'));
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(port);
 console.log('Server running on port: ' + port);
+
+module.exports = app;
--- serveur/config/database.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = {
-    'url': 'mongodb+srv://jrcrenn:aA92rR120@cluster0-1h8gv.mongodb.net/test?retryWrites=true&w=majority'
-}
