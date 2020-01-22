57cdce65314ac89048b6417692e1cc9aa2b1cd19
Jean-Robert CRENN <jean-robert.crenn@epitech.eu>
2020-01-22 18:20:42 +0100

    fix

--- serveur/app.js
+++ serveur/app.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 // Mongoose connect
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
       console.log("Connected to mongoDB");
     })
